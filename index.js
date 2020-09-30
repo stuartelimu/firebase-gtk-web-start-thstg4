1aaa838c09c89b4f0473f60cc8f6fee541f624f0
stuartelimu <stuartelimu@gmail.com>
2020-09-30 05:41:27 +0300

    add user sign-in (RSVP)

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ var guestbookListener = null;
 async function main() {
 
   // Add Firebase project configuration object here
+  var firebaseConfig = {
+    apiKey: "AIzaSyD0ur1zf8B8EjUraK0nwnM7gjM_-FDENMw",
+    authDomain: "fir-web-codelab-e04ff.firebaseapp.com",
+    databaseURL: "https://fir-web-codelab-e04ff.firebaseio.com",
+    projectId: "fir-web-codelab-e04ff",
+    storageBucket: "fir-web-codelab-e04ff.appspot.com",
+    messagingSenderId: "650091709903",
+    appId: "1:650091709903:web:0a2464404b4c9e14acbeba"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
   // var firebaseConfig = {};
 
   // firebase.initializeApp(firebaseConfig);
@@ -46,7 +58,27 @@ async function main() {
     }
   };
 
-  // const ui = new firebaseui.auth.AuthUI(firebase.auth());
-}
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  // listen to RSVP button clicks
+  startRsvpButton.addEventListener('click', () => {
+    if (firebase.auth().currentUser) {
+      // user is signed in, allow the user to sign out
+      firebase.auth().signOut();
+    } else {
+      // no user is signed in, allow the user to sign in
+      ui.start("#firebaseui-auth-container", uiConfig);
+    }
+  });
+
+  // listen to the current Auth state
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      startRsvpButton.textContent = "LOGOUT";
+    } else {
+      startRsvpButton.textContent = "RSVP";
+    }
+  });
+ }
 main();
 
